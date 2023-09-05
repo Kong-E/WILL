3f494e28493b23f1c700d7d8b43781250127d2b5
Kong-E <kng001016@dgu.ac.kr>
2023-09-05 17:35:27 +0900

    chore: 프리티어 추가

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,50 +1,33 @@
-import React from "react";
-import styles from "./Nav.module.css";
+import React from 'react';
+import styles from './Nav.module.css';
 
 export const Nav = () => {
   return (
-      <header className={styles.nav}>
-        <p
-          className={styles.text}
-          style={{
-            fontSize: "28px",
-            color: "#000",
-            marginLeft: "58px",
-          }}
-        >
-          별세
-          <span
-            className={styles.text}
-            style={{ fontSize: "25px", padding: "0px 0px", color: "#000" }}
-          >
-            (別世)
-          </span>
-        </p>
-        <div className={styles.bannerContainer}>
-          <p className={styles.text}>
-            서비스 소개
+    <header className={styles.nav}>
+      <p
+        className={styles.text}
+        style={{
+          fontSize: '28px',
+          color: '#000',
+          marginLeft: '58px',
+        }}>
+        별세
+        <span className={styles.text} style={{ fontSize: '25px', padding: '0px 0px', color: '#000' }}>
+          (別世)
+        </span>
+      </p>
+      <div className={styles.bannerContainer}>
+        <p className={styles.text}>서비스 소개</p>
+        <p className={styles.text}>유언장 작성하기</p>
+        <p className={styles.text}>유언장 찾기</p>
+        <p className={styles.text}>변호사 알아보기</p>
+        <p className={styles.text}>법률 문의 게시판</p>
+        <button className={styles.button}>
+          <p className={styles.text} style={{ color: '#fff', padding: 'px 17px' }}>
+            로그인/회원가입
           </p>
-          <p className={styles.text}>
-            유언장 작성하기
-          </p>
-          <p className={styles.text}>
-            유언장 찾기
-          </p>
-          <p className={styles.text}>
-            변호사 알아보기
-          </p>
-          <p className={styles.text}>
-            법률 문의 게시판
-          </p>
-          <button className={styles.button}>
-            <p
-              className={styles.text}
-              style={{ color: "#fff", padding: "px 17px" }}
-            >
-              로그인/회원가입
-            </p>
-          </button>
-        </div>
-      </header>
+        </button>
+      </div>
+    </header>
   );
 };
