f709a32bef8e3b9cffc8e2e04bb79f4fcb0a1103
Kong-E <kng001016@dgu.ac.kr>
2023-09-05 21:59:04 +0900

    chore: 프리티어 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import GlobalStyle from './GlobalStyle';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './Routes/Layout';
+import { RecoilRoot } from 'recoil';
 import {
   Home,
   Intro,
@@ -18,30 +19,34 @@ import {
   LawyerReview,
   LawyerProfile,
 } from './page';
+import SignUp from 'page/SignUp/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/intro" element={<Intro />} />
-          <Route path="/searching" element={<Searching />} />
-          <Route path="/searchResult" element={<SearchResult />} />
-          <Route path="/writing1" element={<Writing1 />} />
-          <Route path="/writing2" element={<Writing2 />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/myPage" element={<MyPage />} />
-          <Route path="/lawyerMain" element={<LawyerMain />} />
-          <Route path="/lawyerReview" element={<LawyerReview />} />
-          <Route path="/lawyerProfile" element={<LawyerProfile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/intro" element={<Intro />} />
+            <Route path="/searching" element={<Searching />} />
+            <Route path="/searchResult" element={<SearchResult />} />
+            <Route path="/writing1" element={<Writing1 />} />
+            <Route path="/writing2" element={<Writing2 />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/myPage" element={<MyPage />} />
+            <Route path="/lawyerMain" element={<LawyerMain />} />
+            <Route path="/lawyerReview" element={<LawyerReview />} />
+            <Route path="/lawyerProfile" element={<LawyerProfile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>,
 );
 // If you want your app to work offline and load faster, you can change
--- frontend/src/page/Login/Login.jsx
+++ frontend/src/page/Login/Login.jsx
@@ -1,13 +1,80 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import instance from '../../api/axios';
+import { LoginState, UserState } from '../../stores/login-store';
+import { useSetRecoilState } from 'recoil';
+
 export const Login = () => {
+  // Get the setter for LoginState and UserState atoms
+  const setIsLoggedIn = useSetRecoilState(LoginState);
+  const setUserState = useSetRecoilState(UserState);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = async event => {
+    event.preventDefault();
+
+    try {
+      const tokenResponse = await instance.post(
+        '/api/auth/signin',
+        {
+          email: email,
+          password: password,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      const { token } = tokenResponse.data;
+      const currentDate = new Date().getTime();
+      const expirationDate = new Date(currentDate + 60 * 60 * 1000);
+      localStorage.setItem('token', token);
+      localStorage.setItem('expirationDate', expirationDate.toString());
+      console.log('로그인 성공:', tokenResponse.data);
+
+      // 유저 정보 받아오기
+      const userResponse = await instance.get('/api/mypage', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const user = userResponse.data;
+      console.log('유저정보:', user);
+
+      // 로그인 성공 후
+      setIsLoggedIn(true);
+      setUserState(user);
+
+      navigate('/');
+      window.location.reload();
+    } catch (error) {
+      console.error('로그인 실패:', error.response.data.reason);
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
       <label htmlFor="email">Email</label>
-      <input type="email" id="email" />
+      <input type="email" id="email" value={email} onChange={handleEmailChange} />
       <label htmlFor="password">Password</label>
-      <input type="password" id="password" />
-      <button type="submit">Login</button>
-      <button type="button">Register</button>
+      <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+      <button type="submit" onClick={handleLogin}>
+        Login
+      </button>
+      <button type="button" onClick={() => navigate('/signup')}>
+        Register
+      </button>
     </div>
   );
 };
