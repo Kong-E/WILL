88beb0417d9cf56ce97501c915c55a49abd53db5
memymini <133033494+memymini@users.noreply.github.com>
2023-09-07 14:51:27 +0900

    소개페이지-중간

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./Nav.module.css";
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
   return (
-      <header className={styles.nav}>
+    <header className={styles.nav}>
+      <Link to="../../../page/Home" className={styles.link}>
         <p
           className={styles.text}
           style={{
@@ -20,31 +22,34 @@ export const Nav = () => {
             (別世)
           </span>
         </p>
-        <div className={styles.bannerContainer}>
-          <p className={styles.text}>
-            서비스 소개
-          </p>
-          <p className={styles.text}>
-            유언장 작성하기
-          </p>
-          <p className={styles.text}>
-            유언장 찾기
-          </p>
-          <p className={styles.text}>
-            변호사 알아보기
-          </p>
-          <p className={styles.text}>
-            법률 문의 게시판
-          </p>
+      </Link>
+      <div className={styles.bannerContainer}>
+        <Link to="../../../page/Intro">
+          <p className={styles.text}>서비스 소개</p>
+        </Link>
+        <Link to="../../../page/Writing1">
+          <p className={styles.text}>유언장 작성하기</p>
+        </Link>
+        <Link to="../../../page/Searching">
+          <p className={styles.text}>유언장 찾기</p>
+        </Link>
+        <Link to="../../../page/LawyerMain">
+          <p className={styles.text}>변호사 알아보기</p>
+        </Link>
+        <Link to="../../../page/LawyerReview">
+          <p className={styles.text}>법률 문의 게시판</p>
+        </Link>
+        <Link to="../../../page/Login">
           <button className={styles.button}>
             <p
               className={styles.text}
-              style={{ color: "#fff", padding: "px 17px" }}
+              style={{ color: "#fff", padding: "5px 17px" }}
             >
               로그인/회원가입
             </p>
           </button>
-        </div>
-      </header>
+        </Link>
+      </div>
+    </header>
   );
 };
