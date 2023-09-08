3fb298bfc76d33bb765560776c1d33fa2b07fa24
Kong-E <kng001016@dgu.ac.kr>
2023-09-09 02:11:04 +0900

    feat: 음성녹음 기능 구현

--- frontend/src/page/Writing6/Writing6.jsx
+++ frontend/src/page/Writing6/Writing6.jsx
@@ -1,11 +1,72 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 import styles from './Writing6.module.scss';
 import { PageNavigation, Progress } from 'components';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { WillState } from 'stores/will-store';
 
+const mimeType = 'audio/webm';
+
 export const Writing6 = () => {
   const [willState, setWillState] = useRecoilState(WillState);
+  const [permission, setPermission] = useState(false);
+  const mediaRecorder = useRef(null);
+  const [recordingStatus, setRecordingStatus] = useState('inactive');
+  const [stream, setStream] = useState(null);
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [audio, setAudio] = useState(null);
+
+  const getMicrophonePermission = async () => {
+    if ('MediaRecorder' in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+        setPermission(true);
+        setStream(streamData);
+      } catch (err) {
+        alert(err.message);
+      }
+    } else {
+      alert('The MediaRecorder API is not supported in your browser.');
+    }
+  };
+
+  const startRecording = async () => {
+    setRecordingStatus('recording');
+    //create new Media recorder instance using the stream
+    const media = new MediaRecorder(stream, { type: mimeType });
+    //set the MediaRecorder instance to the mediaRecorder ref
+    mediaRecorder.current = media;
+    //invokes the start method to start the recording process
+    mediaRecorder.current.start();
+    let localAudioChunks = [];
+    mediaRecorder.current.ondataavailable = event => {
+      if (typeof event.data === 'undefined') return;
+      if (event.data.size === 0) return;
+      localAudioChunks.push(event.data);
+    };
+    setAudioChunks(localAudioChunks);
+  };
+
+  const stopRecording = () => {
+    setRecordingStatus('inactive');
+    //stops the recording instance
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      //creates a blob file from the audiochunks data
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      //creates a playable URL from the blob file.
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudio(audioUrl);
+      setAudioChunks([]);
+    };
+  };
+
+  useEffect(() => {
+    getMicrophonePermission();
+  }, []);
+
   return (
     <>
       <Progress step={5} />
@@ -21,8 +82,26 @@ export const Writing6 = () => {
           </div>
         </div>
         <div className={styles.record_container}>
-          <img className={styles.record_img} src="/images/record.svg" alt="record" />
-          <div className={styles.record_button}>녹음 시작하기</div>
+          <div
+            className={recordingStatus === 'recording' ? `${styles.record_img} ${styles.active}` : styles.record_img}
+          />
+          {recordingStatus === 'recording' ? (
+            <div className={`${styles.record_button} ${styles.active}`} onClick={stopRecording}>
+              녹음 완료하기
+            </div>
+          ) : (
+            <div className={styles.record_button} onClick={startRecording}>
+              녹음 시작하기
+            </div>
+          )}
+          {audio && (
+            <>
+              <audio className={styles.audio} src={audio} controls></audio>
+              {/* <a download href={audio}>
+                Download Recording
+              </a> */}
+            </>
+          )}
           <div className={styles.text}>녹음이 시작되면 유언장 내용을 읽어주세요.</div>
         </div>
         <div className={styles.will_textarea}>
